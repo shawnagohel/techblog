5176554173b1ed3fd99123e596ae9138dc3e11f6
shawnagohel <shawna.gohel@gmail.com>
2020-11-11 10:27:41 -0800

    refactoring code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const session = require("express-session");
 const handlebars = require("express-handlebars");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const bodyparser = require("body-parser");
 
 const sq = require("./config/connection");
 
@@ -13,9 +14,11 @@ const app = express();
 
 // Import models
 const { Comment, Post, User } = require("./models");
+const { runInNewContext } = require("vm");
 
 const PORT = process.env.PORT || 3000;
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     key: "user",
@@ -39,9 +42,6 @@ var sessionChecker = (req, res, next) => {
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, "../public/")));
 
 // homepage rendering (displays the homepage)
@@ -51,27 +51,64 @@ app.get("/", async function (req, res) {
 
 // displays the dashboard, sends the current user to the dashbaord
 app.get("/dashboard", sessionChecker, function (req, res) {
-  res.render("dashboard", { username: req.session.user })
+  res.render("dashboard", { username: req.session.user });
 });
 
 // fetches and sends all the posts
 app.get("/posts", async function (req, res) {
-  const posts = await Post.findAll()
-  res.send({ posts })
+  const posts = await Post.findAll();
+  res.send({ posts });
 });
+
 
 // adds a post
 app.post("/posts", sessionChecker, async function (req, res) {
-  // implement this now
-  res.send({});
+  console.log("creating post...");
+  console.log(req.body);
+  try {
+    const { title, text } = req.body;
+    const result = await Post.create({
+      title: title,
+      body: text,
+      user_id: req.session.user,
+    });
+    if (result) res.status(200);
+  } catch (e) {
+    console.log('error creating post')
+    res.status(500);
+  }
 });
+
+
+// updates a post
+app.post("/posts/:id", async (req, res) => {
+  try {
+    let { id, title, text } = req.body;
+
+    id = parseInt(id);
+
+    const result = await Post.update(
+      { title: title, body: text },
+      { where: { id: req.params.id } }
+    );
+
+    if (result) {
+      res.status(200);
+      res.end();
+    }
+  } catch (e) {
+    res.status(500);
+    console.log(e);
+  }
+});
+
 
 // checks if a user is already logged in
 // if they are logged in redirect them to the homepage
 // or else send them to the login page
 app.get("/login", async function (req, res) {
-  if (!req.session.user) // is the user not signed in?
-   {
+  if (!req.session.user) {
+    // is the user not signed in?
     res.render("login");
   } else res.redirect("/");
 });
@@ -112,7 +149,8 @@ app.post("/users", async function (req, res) {
 
   const existingUser = await User.findOne({ where: { email: username } });
 
-  if (!existingUser) { // if there is no existing user
+  if (!existingUser) {
+    // if there is no existing user
     const user = await User.create({
       email: username,
       password: password,
@@ -126,7 +164,7 @@ app.post("/users", async function (req, res) {
 });
 
 // returns an array of posts for a specific user
-app.get("/posts/:name", async (req, res) => {
+app.get("/posts/user/:name", async (req, res) => {
   if (req.params.name === req.session.user) {
     const posts = await Post.findAll({ where: { user_id: req.params.name } });
     res.json({
@@ -137,6 +175,39 @@ app.get("/posts/:name", async (req, res) => {
     res.status(403);
   }
 });
+
+// ============================================
+// COMMENT ROUTES
+// ============================================
+
+app.post('/comments/', sessionChecker, async (req, res) => {
+  try {
+
+    const { post_id, comment } = req.body
+
+    Comment.create( { user_id: req.session.user, post_id: post_id, comment: comment  } )
+  }
+  catch (e) {
+    console.log(`ERROR CREATING COMMENT ${ e }`)
+  }
+})
+
+app.get('/comments/:id', async (req, res) => {
+
+    try {
+      const comments = Comment.findAll({where: { post_id: req.params.id } })
+
+      res.json({
+        comments
+      })
+
+    }
+    catch (e) {
+      console.log(e);
+    }
+
+})
+
 
 sq.sync();
 
